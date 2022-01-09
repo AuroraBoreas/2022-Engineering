c06cc29b4d7c51db497ae445a860f181aedc2e2b
ZL <34211023+AuroraBoreas@users.noreply.github.com>
2022-01-09 17:56:37 +0800

    Add files via upload

---
+++ 20220109MIT_PhysicsI_ClassicalMechanics/doc/readme.md
@@ -0,0 +1,3 @@
+# MIT PhysicsI Classical Mechanics
+
+this project summarizes all info and converts formulas into python libraries;
---
+++ 20220109MIT_PhysicsI_ClassicalMechanics/lect01/uncertainty.py
@@ -0,0 +1,113 @@
+"""
+
+this module is to calculate meansurement uncertainty.
+
+## Why
+Walter Lewin @MIT, 2015:
+"Any measurement you make w/o the knowledge of its uncertainty is meaningless;
+
+## What
+what is measurement uncerntainty?
+whenever you make a measurement while collecting data,
+you can assume that there is a "true value"
+that falls within the range of the measurements you made.
+
+you need to find the best estimate of your measurement and consider the results
+when you add or substract the measurement uncertainty.
+
+## How
+how to perform airthmetic oprations?
+link: https://www.wikihow.com/Calculate-Uncertainty
+
+## Author
+@ZL, 20220109
+
+## Changelog
+- v0.01, initial build
+
+"""
+
+from __future__ import annotations
+from decimal import Decimal
+from functools import total_ordering
+
+@total_ordering
+class Measurement:
+    _meas_val:Decimal = None # measurement value, unit is same by default
+    _meas_unc:Decimal = None # measurement uncertainty, unit is same by default
+
+    def __init__(self, mv:Decimal, mu:Decimal) -> None:
+        """init object with measurement value, measurement uncertainty
+
+        Args:
+            mv (Decimal): measurement value
+            mu (Decimal): measurement uncertainty
+
+        Raises:
+            ValueError: the decimal places of measurement value and uncertainty must match
+        """
+        if mv.as_tuple().exponent != mv.as_tuple().exponent:
+            raise ValueError("measurement value and uncertainty must have same decimal places, for example: 3.14 ± 0.12")
+        self._meas_val = mv
+        self._meas_unc = mu
+
+    def __str__(self) -> str:
+        return f'absolute: ({self._meas_val} ± {self._meas_unc}); relative: ({self.to_relative()})'
+
+    @property
+    def relative_uncertainty(self)->float:
+        return self._meas_unc / self._meas_val
+
+    def to_relative(self)->str:
+        return f'{self._meas_val} ± {self.relative_uncertainty * 100}%'
+
+    def __eq__(self, other: Measurement) -> bool:
+        return self._meas_val == other._meas_val
+
+    def __le__(self, other: Measurement) -> bool:
+        return self._meas_val < other._meas_val
+
+    def __add__(self, other:Measurement)->Measurement:
+        return Measurement(self._meas_val + other._meas_val, self._meas_unc + other._meas_unc)
+
+    def __sub__(self, other:Measurement)->Measurement:
+        return Measurement(self._meas_val - other._meas_val, self._meas_unc + other._meas_unc)
+
+    def __mul__(self, other:Measurement)->Measurement:
+        new_mv = self._meas_val * other._meas_val
+        return Measurement(new_mv, new_mv * (self.relative_uncertainty + other.relative_uncertainty))
+
+    def __truediv__(self, other:Measurement)->Measurement:
+        new_mv = self._meas_val / other._meas_val
+        return Measurement(new_mv, new_mv * (self.relative_uncertainty + other.relative_uncertainty))
+
+    def pow(self, p:int)->Measurement:
+        # new_mv = math.pow(self._meas_val, p) not work, using dunder method Decimal.__pow__() instead
+        new_mv = self._meas_val.__pow__(p)
+        new_mu = self.relative_uncertainty * p
+        return Measurement(new_mv, new_mv * new_mu)
+
+def client_code()->None:
+    mu1 = Measurement(Decimal('5.0'), Decimal('.2'))
+    mu2 = Measurement(Decimal('3.0'), Decimal('.1'))
+    print(mu1 + mu2)
+    print(mu1 < mu2)
+
+    mu1 = Measurement(Decimal('10.0'), Decimal('.4'))
+    mu2 = Measurement(Decimal('3.0'), Decimal('.2'))
+    print(mu1 - mu2)
+
+    mu1 = Measurement(Decimal('6.0'), Decimal('.2'))
+    mu2 = Measurement(Decimal('4.0'), Decimal('.3'))
+    print(mu1 * mu2)
+
+    mu1 = Measurement(Decimal('10.0'), Decimal('.6'))
+    mu2 = Measurement(Decimal('5.0'), Decimal('.2'))
+    print(mu1 / mu2)
+
+    mu1 = Measurement(Decimal('2.0'), Decimal('1.0'))
+    print(mu1.pow(3))
+    print(mu1 == mu1)
+
+if __name__ == '__main__':
+    client_code()
